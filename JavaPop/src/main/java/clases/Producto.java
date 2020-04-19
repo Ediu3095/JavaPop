f668fa87280f3199cb160d13000bea12ea07384f
Ediu3095 <3arcintel@gmail.com>
2020-04-19 17:44:50 +0200

    Creada clase producto

---
+++ JavaPop/src/main/java/clases/Producto.java
@@ -0,0 +1,19 @@
+package clases;
+
+import clases.enumeradores.*;
+import java.awt.Image;
+import java.time.LocalDateTime;
+
+public class Producto {
+
+    protected String titulo;
+    protected String descripcion;
+    protected Categoria categoria;
+    protected Estado estado;
+    protected double precio;
+    protected Image foto;
+    protected LocalDateTime fechaPublicacion;
+    protected int codigoPostalVendedor;
+    protected boolean urgente;
+    
+}
