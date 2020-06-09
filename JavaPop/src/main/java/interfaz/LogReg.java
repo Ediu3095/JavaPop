1cfde8e1751bd93933cc51c0f73adbfa95f8d51d
luismisobrino <luismisobrino97@gmail.com>
2020-06-09 18:40:49 +0200

    Update LogReg.java

--- JavaPop/src/main/java/interfaz/LogReg.java
+++ JavaPop/src/main/java/interfaz/LogReg.java
@@ -275,7 +275,7 @@ public class LogReg extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+
     }//GEN-LAST:event_jButton2ActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
